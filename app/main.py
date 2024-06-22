0a49062104ad2dea3707bdcf2dae67d8c083ce64
Luciana R Lirancos <91036903+LuhLirancos@users.noreply.github.com>
2024-06-22 00:50:39 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+## Tentei tirar o main do projeto e colocar direto no controler, mas aconteceu um erro por não conseguir rodar da outra forma mantive o arquivo. 
+
 from fastapi import FastAPI
 from app.controllers import task_controller
 
